1e3cf5151da22f2d485bd5b2e7d07eaa7cc51114
adybag14-cyber <adybag14@gmail.com>
2026-04-26 11:50:44 +0100

    phase6: consolidate hexdump fixture helpers

--- zigux/tests/fixtures/phase6_hexdump_vectors.zig
+++ zigux/tests/fixtures/phase6_hexdump_vectors.zig
@@ -1,4 +1,5 @@
 const builtin = @import("builtin");
+const std = @import("std");
 
 pub const data_b = [_]u8{
     0xbe, 0x32, 0xdb, 0x7b, 0x0a, 0x18, 0x93, 0xb2,
@@ -43,6 +44,118 @@ pub const OverflowCase = struct {
 
 fn same(text: []const u8) ExpectedText {
     return .{ .little = text, .big = text };
+}
+
+const test_data_1 = [_][]const u8{
+    "be", "32", "db", "7b", "0a", "18", "93", "b2",
+    "70", "ba", "c4", "24", "7d", "83", "34", "9b",
+    "a6", "9c", "31", "ad", "9c", "0f", "ac", "e9",
+    "4c", "d1", "19", "99", "43", "b1", "af", "0c",
+};
+
+const test_data_2_le = [_][]const u8{
+    "32be", "7bdb", "180a", "b293",
+    "ba70", "24c4", "837d", "9b34",
+    "9ca6", "ad31", "0f9c", "e9ac",
+    "d14c", "9919", "b143", "0caf",
+};
+
+const test_data_2_be = [_][]const u8{
+    "be32", "db7b", "0a18", "93b2",
+    "70ba", "c424", "7d83", "349b",
+    "a69c", "31ad", "9c0f", "ace9",
+    "4cd1", "1999", "43b1", "af0c",
+};
+
+const test_data_4_le = [_][]const u8{
+    "7bdb32be", "b293180a", "24c4ba70", "9b34837d",
+    "ad319ca6", "e9ac0f9c", "9919d14c", "0cafb143",
+};
+
+const test_data_4_be = [_][]const u8{
+    "be32db7b", "0a1893b2", "70bac424", "7d83349b",
+    "a69c31ad", "9c0face9", "4cd11999", "43b1af0c",
+};
+
+const test_data_8_le = [_][]const u8{
+    "b293180a7bdb32be", "9b34837d24c4ba70",
+    "e9ac0f9cad319ca6", "0cafb1439919d14c",
+};
+
+const test_data_8_be = [_][]const u8{
+    "be32db7b0a1893b2", "70bac4247d83349b",
+    "a69c31ad9c0face9", "4cd1199943b1af0c",
+};
+
+pub fn normalizedRowsize(rowsize_input: usize) usize {
+    return if (rowsize_input == 16 or rowsize_input == 32) rowsize_input else 16;
+}
+
+pub fn normalizedGroupsizeForLen(len: usize, groupsize_input: usize) usize {
+    var groupsize = groupsize_input;
+    if (!std.math.isPowerOfTwo(groupsize) or groupsize > 8 or groupsize == 0) {
+        groupsize = 1;
+    }
+    if (len % groupsize != 0) {
+        groupsize = 1;
+    }
+    return groupsize;
+}
+
+fn fixtureChunks(groupsize: usize) []const []const u8 {
+    return switch (groupsize) {
+        8 => if (builtin.cpu.arch.endian() == .big) test_data_8_be[0..] else test_data_8_le[0..],
+        4 => if (builtin.cpu.arch.endian() == .big) test_data_4_be[0..] else test_data_4_le[0..],
+        2 => if (builtin.cpu.arch.endian() == .big) test_data_2_be[0..] else test_data_2_le[0..],
+        else => test_data_1[0..],
+    };
+}
+
+pub fn prepareExpectedLine(
+    buffer: []u8,
+    len_input: usize,
+    rowsize_input: usize,
+    groupsize_input: usize,
+    ascii: bool,
+) []const u8 {
+    const rowsize = normalizedRowsize(rowsize_input);
+    const len = @min(len_input, rowsize);
+    const groupsize = normalizedGroupsizeForLen(len, groupsize_input);
+    const chunks = fixtureChunks(groupsize);
+
+    var pos: usize = 0;
+    var index: usize = 0;
+    while (index < len / groupsize) : (index += 1) {
+        const chunk = chunks[index];
+        @memcpy(buffer[pos .. pos + chunk.len], chunk);
+        pos += chunk.len;
+        buffer[pos] = ' ';
+        pos += 1;
+    }
+    if (index != 0) {
+        pos -= 1;
+    }
+
+    if (ascii) {
+        while (pos < rowsize * 2 + rowsize / groupsize + 1) : (pos += 1) {
+            buffer[pos] = ' ';
+        }
+        @memcpy(buffer[pos .. pos + len], data_a[0..len]);
+        pos += len;
+    }
+
+    buffer[pos] = 0;
+    return buffer[0..pos];
+}
+
+pub fn expectedLength(len_input: usize, rowsize_input: usize, groupsize_input: usize, ascii: bool) usize {
+    const rowsize = normalizedRowsize(rowsize_input);
+    const len = @min(len_input, rowsize);
+    const groupsize = normalizedGroupsizeForLen(len, groupsize_input);
+    if (ascii) {
+        return rowsize * 2 + rowsize / groupsize + 1 + len;
+    }
+    return if (len == 0) 0 else (groupsize * 2 + 1) * (len / groupsize) - 1;
 }
 
 pub const parity_cases = [_]ParityCase{
