3ee65361f2d3f0900a1e35594dcc825d31700a0c
adybag14-cyber <adybag14@gmail.com>
2026-04-29 15:17:37 +0100

    feat(zigux): tighten phase3 canonical layout asserts

--- zigux/helpers/layout_assert.zig
+++ zigux/helpers/layout_assert.zig
@@ -29,16 +29,35 @@ pub fn assertOffset(comptime T: type, comptime field_name: []const u8, comptime 
     }
 }
 
+pub fn assertBoundaryHeaderLayout() void {
+    assertSize(abi.BoundaryHeader, 8);
+    assertAlign(abi.BoundaryHeader, 4);
+    assertOffset(abi.BoundaryHeader, "size", 0);
+    assertOffset(abi.BoundaryHeader, "abi_version", 4);
+    assertOffset(abi.BoundaryHeader, "flags", 6);
+}
+
+pub fn assertExportStatusLayout() void {
+    assertSize(abi.ExportStatus, 8);
+    assertAlign(abi.ExportStatus, 4);
+    assertOffset(abi.ExportStatus, "code", 0);
+    assertOffset(abi.ExportStatus, "facility", 4);
+    assertOffset(abi.ExportStatus, "flags", 6);
+}
+
+pub fn assertInteropPolicyLayout() void {
+    assertSize(abi.InteropPolicy, 4);
+    assertAlign(abi.InteropPolicy, 1);
+    assertOffset(abi.InteropPolicy, "panic_mode", 0);
+    assertOffset(abi.InteropPolicy, "allocator_mode", 1);
+    assertOffset(abi.InteropPolicy, "unsafe_scope", 2);
+    assertOffset(abi.InteropPolicy, "reserved", 3);
+}
+
 test "phase3 layout assertions cover canonical bindings" {
     comptime {
-        assertSize(abi.BoundaryHeader, 8);
-        assertAlign(abi.BoundaryHeader, 4);
-        assertOffset(abi.BoundaryHeader, "abi_version", 4);
-        assertOffset(abi.BoundaryHeader, "flags", 6);
-        assertOffset(abi.ExportStatus, "facility", 4);
-        assertOffset(abi.ExportStatus, "flags", 6);
-        assertSize(abi.InteropPolicy, 4);
-        assertOffset(abi.InteropPolicy, "allocator_mode", 1);
-        assertOffset(abi.InteropPolicy, "unsafe_scope", 2);
+        assertBoundaryHeaderLayout();
+        assertExportStatusLayout();
+        assertInteropPolicyLayout();
     }
 }
