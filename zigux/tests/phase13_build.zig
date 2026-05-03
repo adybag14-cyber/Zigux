3ba5d43db03b9bfe715d78b3886abd9233b83abf
adybag14-cyber <adybag14@gmail.com>
2026-05-03 20:56:43 +0100

    test(zigux): wire landlock ruleset reviewability into phase13 build

--- zigux/tests/phase13_build.zig
+++ zigux/tests/phase13_build.zig
@@ -65,6 +65,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     phase13_landlock_ruleset_module.addImport("landlock_ruleset", landlock_ruleset_module);
+    const phase13_landlock_ruleset_reviewability_module = b.createModule(.{
+        .root_source_file = b.path("phase13_landlock_ruleset_reviewability.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    phase13_landlock_ruleset_reviewability_module.addImport("landlock_ruleset", landlock_ruleset_module);
     const phase13_landlock_syscalls_module = b.createModule(.{
         .root_source_file = b.path("phase13_landlock_syscalls.zig"),
         .target = target,
@@ -142,6 +148,11 @@ pub fn build(b: *std.Build) void {
         .root_module = phase13_landlock_ruleset_module,
     });
     const run_phase13_landlock_ruleset_tests = b.addRunArtifact(phase13_landlock_ruleset_tests);
+    const phase13_landlock_ruleset_reviewability_tests = b.addTest(.{
+        .name = "phase13-landlock-ruleset-reviewability-tests",
+        .root_module = phase13_landlock_ruleset_reviewability_module,
+    });
+    const run_phase13_landlock_ruleset_reviewability_tests = b.addRunArtifact(phase13_landlock_ruleset_reviewability_tests);
     const phase13_landlock_syscalls_tests = b.addTest(.{
         .name = "phase13-landlock-syscalls-tests",
         .root_module = phase13_landlock_syscalls_module,
@@ -180,6 +191,7 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_phase13_devres_iounmap_reviewability_tests.step);
     test_step.dependOn(&run_phase13_devres_iomap_reviewability_tests.step);
     test_step.dependOn(&run_phase13_landlock_ruleset_tests.step);
+    test_step.dependOn(&run_phase13_landlock_ruleset_reviewability_tests.step);
     test_step.dependOn(&run_phase13_landlock_syscalls_tests.step);
     test_step.dependOn(&run_phase13_landlock_syscalls_reviewability_tests.step);
     test_step.dependOn(&run_phase13_libfs_reviewability_tests.step);
