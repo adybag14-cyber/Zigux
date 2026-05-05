42f05725beb3d169865ac648655f28021709b8c2
adybag14-cyber <adybag14@gmail.com>
2026-05-05 05:39:31 +0100

    feat(zigux): restore bounded phase3 barrier helper surface

--- zigux/helpers/barrier.zig
+++ zigux/helpers/barrier.zig
@@ -1,19 +1,28 @@
-var fence_word: u8 = 0;
+fn fenceProbe() *u8 {
+    var word: u8 = 0;
+    return &word;
+}
+
+pub fn acquire() void {
+    _ = @atomicLoad(u8, fenceProbe(), .acquire);
+}
+
+pub fn release() void {
+    @atomicStore(u8, fenceProbe(), 0, .release);
+}
+
+pub fn full() void {
+    _ = @atomicRmw(u8, fenceProbe(), .Xchg, 0, .seq_cst);
+}
+
+pub fn acquireRelease() void {
+    _ = @atomicLoad(u8, fenceProbe(), .acquire);
+    @atomicStore(u8, fenceProbe(), 0, .release);
+}
 
 test "phase3 barrier wrappers compile" {
     acquire();
     release();
     full();
-}
-
-pub fn acquire() void {
-    _ = @atomicLoad(u8, &fence_word, .acquire);
-}
-
-pub fn release() void {
-    @atomicStore(u8, &fence_word, fence_word, .release);
-}
-
-pub fn full() void {
-    _ = @atomicRmw(u8, &fence_word, .Xchg, fence_word, .seq_cst);
+    acquireRelease();
 }
