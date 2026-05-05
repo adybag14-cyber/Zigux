4d75053b9a1e3648fa215c22629f92bb3cbb580f
adybag14-cyber <adybag14@gmail.com>
2026-05-05 09:03:40 +0100

    test(zigux): add phase11 hvc cleanup replay

---
+++ zigux/tests/phase11_hvc_cleanup.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+const hvc_console = @import("hvc_console");
+
+test "phase11 hvc console keeps hvc_cleanup tty-port release boundaries reviewable" {
+    var console = try hvc_console.HvcConsoleLab.init(5);
+    _ = console.instantiate(0x55);
+
+    const final_cleanup = try console.summarizeCleanupHandoff(.{});
+    try std.testing.expectEqual(@as(usize, 5), final_cleanup.slot_index);
+    try std.testing.expectEqual(@as(u32, 0x55), final_cleanup.vtermno);
+    try std.testing.expect(final_cleanup.adapter_present);
+    try std.testing.expect(!final_cleanup.close_skipped);
+    try std.testing.expect(final_cleanup.final_close);
+    try std.testing.expect(final_cleanup.tty_port_put_requested);
+    try std.testing.expect(final_cleanup.drops_tty_port_reference);
+    try std.testing.expect(final_cleanup.deferred_final_release);
+
+    const hangup_cleanup = try console.summarizeCleanupHandoff(.{
+        .hung_up = true,
+        .final_close = false,
+    });
+    try std.testing.expect(hangup_cleanup.close_skipped);
+    try std.testing.expect(!hangup_cleanup.final_close);
+    try std.testing.expect(hangup_cleanup.tty_port_put_requested);
+    try std.testing.expect(hangup_cleanup.drops_tty_port_reference);
+    try std.testing.expect(hangup_cleanup.deferred_final_release);
+
+    try std.testing.expectError(error.CleanupRequiresFinalCloseOrHangup, console.summarizeCleanupHandoff(.{
+        .final_close = false,
+    }));
+
+    _ = console.teardown();
+    try std.testing.expectError(error.ConsoleUnavailable, console.summarizeCleanupHandoff(.{}));
+}
