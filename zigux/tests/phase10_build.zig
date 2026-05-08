4c7eb1a6b5441615879319f3e35262aeb1431d4c
adybag14-cyber <adybag14@gmail.com>
2026-05-08 17:18:49 +0100

    phase10: wire virtio input teardown replay into shared build

--- zigux/tests/phase10_build.zig
+++ zigux/tests/phase10_build.zig
@@ -98,6 +98,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     phase10_virtio_input_registration_preflight_module.addImport("virtio_input", virtio_input_module);
+    const phase10_virtio_input_teardown_observation_module = b.createModule(.{
+        .root_source_file = b.path("phase10_virtio_input_teardown_observation.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    phase10_virtio_input_teardown_observation_module.addImport("virtio_input", virtio_input_module);
     const phase10_virtio_input_verify_module = b.createModule(.{
         .root_source_file = b.path("../../drivers/virtio/virtio_input_verify.zig"),
         .target = target,
@@ -187,6 +193,11 @@ pub fn build(b: *std.Build) void {
         .root_module = phase10_virtio_input_registration_preflight_module,
     });
     const run_phase10_virtio_input_registration_preflight_tests = b.addRunArtifact(phase10_virtio_input_registration_preflight_tests);
+    const phase10_virtio_input_teardown_observation_tests = b.addTest(.{
+        .name = "phase10-virtio-input-teardown-observation-tests",
+        .root_module = phase10_virtio_input_teardown_observation_module,
+    });
+    const run_phase10_virtio_input_teardown_observation_tests = b.addRunArtifact(phase10_virtio_input_teardown_observation_tests);
     const phase10_virtio_input_verify_tests = b.addTest(.{
         .name = "phase10-virtio-input-verify-tests",
         .root_module = phase10_virtio_input_verify_module,
@@ -226,6 +237,7 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_phase10_virtio_input_status_drain_tests.step);
     test_step.dependOn(&run_phase10_virtio_input_queue_callback_preflight_tests.step);
     test_step.dependOn(&run_phase10_virtio_input_registration_preflight_tests.step);
+    test_step.dependOn(&run_phase10_virtio_input_teardown_observation_tests.step);
     test_step.dependOn(&run_phase10_virtio_input_verify_tests.step);
     test_step.dependOn(&run_phase10_virtio_input_survey_tests.step);
     test_step.dependOn(&run_phase10_virtio_mmio_tests.step);
