ba1baeccb7454ac5e4088afce60bae577d5f7402
adybag14-cyber <adybag14@gmail.com>
2026-05-11 04:37:12 +0100

    feat(zigux): add Phase 3 barrier acquireRelease wrapper

---
+++ zigux/helpers/barrier.zig
@@ -0,0 +1,24 @@
+var fence_word: u8 = 0;
+
+test "phase3 barrier wrappers compile" {
+    acquire();
+    release();
+    full();
+    acquireRelease();
+}
+
+pub fn acquire() void {
+    _ = @atomicLoad(u8, &fence_word, .acquire);
+}
+
+pub fn release() void {
+    @atomicStore(u8, &fence_word, fence_word, .release);
+}
+
+pub fn full() void {
+    _ = @atomicRmw(u8, &fence_word, .Xchg, fence_word, .seq_cst);
+}
+
+pub fn acquireRelease() void {
+    _ = @atomicRmw(u8, &fence_word, .Xchg, fence_word, .acq_rel);
+}
