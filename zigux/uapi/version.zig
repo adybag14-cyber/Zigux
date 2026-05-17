3bb4f3eb05694454f9df5b1b9e627b23a78eaa4c
adybag14-cyber <adybag14@gmail.com>
2026-05-17 08:15:14 +0100

    test(zigux): align lane04 dev_t version binding surface

--- zigux/uapi/version.zig
+++ zigux/uapi/version.zig
@@ -10,6 +10,12 @@ pub const Version = extern struct {
     header_family_revision: u32,
 };
 
+pub const version_size: usize = @sizeOf(Version);
+pub const version_align: usize = @alignOf(Version);
+pub const abi_major_offset: usize = @offsetOf(Version, "abi_major");
+pub const abi_minor_offset: usize = @offsetOf(Version, "abi_minor");
+pub const header_family_revision_offset: usize = @offsetOf(Version, "header_family_revision");
+
 pub fn current() Version {
     return .{
         .abi_major = abi_major,
@@ -18,7 +24,16 @@ pub fn current() Version {
     };
 }
 
+pub fn eql(left: Version, right: Version) bool {
+    return left.abi_major == right.abi_major and
+        left.abi_minor == right.abi_minor and
+        left.header_family_revision == right.header_family_revision;
+}
+
 comptime {
-    std.debug.assert(@sizeOf(Version) == 12);
-    std.debug.assert(@alignOf(Version) == 4);
+    std.debug.assert(version_size == 12);
+    std.debug.assert(version_align == 4);
+    std.debug.assert(abi_major_offset == 0);
+    std.debug.assert(abi_minor_offset == 4);
+    std.debug.assert(header_family_revision_offset == 8);
 }
