8387fafe864c0b748fcaddf4bf2ec6aec46ef416
adybag14-cyber <adybag14@gmail.com>
2026-05-17 04:41:06 +0100

    feat(zigux): add starter uapi version relay helpers

--- include/linux/zigux.h
+++ include/linux/zigux.h
@@ -25,4 +25,22 @@ static inline struct zigux_uapi_version zigux_uapi_version_current(void) {
     return version;
 }
 
+static inline int zigux_uapi_version_has_current_abi_major(uint32_t abi_major) {
+    return abi_major == ZIGUX_UAPI_ABI_MAJOR;
+}
+
+static inline int zigux_uapi_version_has_current_abi_minor(uint32_t abi_minor) {
+    return abi_minor == ZIGUX_UAPI_ABI_MINOR;
+}
+
+static inline int zigux_uapi_version_has_current_header_family_revision(uint32_t header_family_revision) {
+    return header_family_revision == ZIGUX_UAPI_HEADER_FAMILY_REVISION;
+}
+
+static inline int zigux_uapi_version_matches_current(struct zigux_uapi_version version) {
+    return zigux_uapi_version_has_current_abi_major(version.abi_major) &&
+        zigux_uapi_version_has_current_abi_minor(version.abi_minor) &&
+        zigux_uapi_version_has_current_header_family_revision(version.header_family_revision);
+}
+
 #endif
