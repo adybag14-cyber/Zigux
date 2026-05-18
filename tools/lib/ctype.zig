23eb9080d722cb306f1376b14e52a23430558d51
adybag14-cyber <adybag14@gmail.com>
2026-05-18 06:20:50 +0100

    test(tools/lib): refresh ctype fastTolower regression anchors

--- tools/lib/ctype.zig
+++ tools/lib/ctype.zig
@@ -124,42 +124,19 @@ test "ctype transforms and ascii helpers behave" {
     try std.testing.expect(!isodigit('8'));
 }
 
-test "ctype extended latin pairs and table-driven invariants stay aligned" {
-    try std.testing.expect(isupper(0xC0));
-    try std.testing.expect(islower(0xE0));
-    try std.testing.expectEqual(@as(u8, 0xE0), tolower(0xC0));
-    try std.testing.expectEqual(@as(u8, 0xC0), toupper(0xE0));
-    try std.testing.expectEqual(@as(u8, 0xF8), fastTolower(0xD8));
+test "fastTolower leaves non-uppercase punctuation unchanged" {
+    try std.testing.expectEqual(@as(u8, '['), fastTolower('['));
+    try std.testing.expectEqual(@as(u8, '\\'), fastTolower('\\'));
+    try std.testing.expectEqual(@as(u8, '^'), fastTolower('^'));
+}
 
-    var ch: u16 = 0;
-    while (ch < 256) : (ch += 1) {
-        const byte: u8 = @intCast(ch);
-        const byte_mask = table[byte];
+test "ctype latin1 table anchors preserve case mapping" {
+    const upper_a_grave: u8 = 0xc0;
+    const lower_a_grave: u8 = 0xe0;
 
-        try std.testing.expectEqual(byte_mask, mask(byte));
-        try std.testing.expectEqual((byte_mask & (_U | _L | _D)) != 0, isalnum(byte));
-        try std.testing.expectEqual((byte_mask & (_U | _L)) != 0, isalpha(byte));
-        try std.testing.expectEqual((byte_mask & _C) != 0, iscntrl(byte));
-        try std.testing.expectEqual((byte_mask & (_P | _U | _L | _D)) != 0, isgraph(byte));
-        try std.testing.expectEqual((byte_mask & _L) != 0, islower(byte));
-        try std.testing.expectEqual((byte_mask & (_P | _U | _L | _D | _SP)) != 0, isprint(byte));
-        try std.testing.expectEqual((byte_mask & _P) != 0, ispunct(byte));
-        try std.testing.expectEqual((byte_mask & _S) != 0, isspace(byte));
-        try std.testing.expectEqual((byte_mask & _U) != 0, isupper(byte));
-        try std.testing.expectEqual((byte_mask & (_D | _X)) != 0, isxdigit(byte));
-        try std.testing.expectEqual(byte <= 0x7f, isascii(byte));
-        try std.testing.expectEqual(@as(u8, byte & 0x7f), toascii(byte));
-
-        if (isupper(byte)) {
-            try std.testing.expectEqual(@as(u8, byte | 0x20), tolower(byte));
-            try std.testing.expectEqual(@as(u8, byte | 0x20), fastTolower(byte));
-            try std.testing.expectEqual(byte, toupper(byte));
-        } else if (islower(byte)) {
-            try std.testing.expectEqual(@as(u8, byte - ('a' - 'A')), toupper(byte));
-            try std.testing.expectEqual(byte, tolower(byte));
-            try std.testing.expectEqual(byte, fastTolower(byte));
-        } else {
-            try std.testing.expectEqual(byte, fastTolower(byte));
-        }
-    }
+    try std.testing.expect(isupper(upper_a_grave));
+    try std.testing.expect(islower(lower_a_grave));
+    try std.testing.expectEqual(lower_a_grave, tolower(upper_a_grave));
+    try std.testing.expectEqual(lower_a_grave, fastTolower(upper_a_grave));
+    try std.testing.expectEqual(upper_a_grave, toupper(lower_a_grave));
 }
