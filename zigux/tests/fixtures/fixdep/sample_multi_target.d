fb535caac1df566bfdd20ddd2a840c721d116b0f
adybag14-cyber <adybag14@gmail.com>
2026-05-18 17:28:39 +0100

    test(zigux): repeat escaped-hash dep in lane13 multi-target fixture

--- zigux/tests/fixtures/fixdep/sample_multi_target.d
+++ zigux/tests/fixtures/fixdep/sample_multi_target.d
@@ -2,5 +2,6 @@ module/sample2.o module/sample2.d: zigux/tests/fixtures/fixdep/sample2.c \
  zigux/tests/fixtures/fixdep/shared\#config.h \
  zigux/tests/fixtures/fixdep/sample2-config.h \
  zigux/tests/fixtures/fixdep/sample2.so \
+ zigux/tests/fixtures/fixdep/shared\#config.h \
  zigux/tests/fixtures/fixdep/sample2-config.h
 # comment line that should be ignored
