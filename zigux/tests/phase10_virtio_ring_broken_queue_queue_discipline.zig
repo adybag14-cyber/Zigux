3a2245ed3df9482f43568987fa13436686562db5
adybag14-cyber <adybag14@gmail.com>
2026-05-18 16:30:27 +0100

    phase10: cover broken virtio ring used accounting fence

--- zigux/tests/phase10_virtio_ring_broken_queue_queue_discipline.zig
+++ zigux/tests/phase10_virtio_ring_broken_queue_queue_discipline.zig
@@ -37,3 +37,33 @@ test "phase10 virtio ring broken-queue coverage kicks published work before used
     try std.testing.expectEqual(@as(u16, 1), second_kick.num_added);
     try std.testing.expectEqual(@as(usize, 2), second_kick.notification_count);
 }
+
+test "phase10 virtio ring broken queues reject used accounting so clearBroken cannot hide outstanding debt" {
+    var ring = virtio_ring.VirtioRingLab{};
+    try ring.defineQueue(4, 8, .packed_ring, true, false);
+    try ring.publishDescriptorChain(4);
+    _ = try ring.prepareKick(4);
+
+    const broken = try ring.markBroken(4);
+    try std.testing.expect(broken.broken);
+    try std.testing.expectEqual(@as(u16, 1), broken.outstanding_chain_count);
+    try std.testing.expectEqual(@as(u16, 0), broken.pending_used_chain_count);
+
+    try std.testing.expectError(error.QueueBroken, ring.recordUsedChains(4, 1));
+
+    const summary_while_broken = try ring.brokenQueueSummary(4);
+    try std.testing.expect(summary_while_broken.broken);
+    try std.testing.expectEqual(@as(u16, 1), summary_while_broken.outstanding_chain_count);
+    try std.testing.expectEqual(@as(u16, 0), summary_while_broken.pending_used_chain_count);
+
+    const cleared = try ring.clearBroken(4);
+    try std.testing.expect(!cleared.broken);
+    try std.testing.expectEqual(@as(u16, 1), cleared.outstanding_chain_count);
+    try std.testing.expectEqual(@as(u16, 0), cleared.pending_used_chain_count);
+
+    const readiness = try ring.queueResetReadinessSummary(4);
+    try std.testing.expect(!readiness.reset_ready);
+    try std.testing.expectEqualStrings("outstanding_chains", @tagName(readiness.blocker.?));
+    try std.testing.expectEqual(@as(u16, 1), readiness.outstanding_chain_count);
+    try std.testing.expectEqual(@as(u16, 0), readiness.pending_used_chain_count);
+}
