25cefc3e57afdd4768be90d9065abcdf67305c23
adybag14-cyber <adybag14@gmail.com>
2026-05-19 18:34:43 +0100

    test(zigux): widen escaped-colon fixdep fixture rmeta parity

--- zigux/tests/fixtures/fixdep/sample_escaped_colon.d
+++ zigux/tests/fixtures/fixdep/sample_escaped_colon.d
@@ -3,4 +3,5 @@ sample_escaped_colon.o: zigux/tests/fixtures/fixdep/sample_escaped_colon_source.
  include/generated/autoconf.h \
  zigux/tests/fixtures/fixdep/sample2.so \
  zigux/tests/fixtures/fixdep/sample.rlib \
+ zigux/tests/fixtures/fixdep/sample.rmeta \
  zigux/tests/fixtures/fixdep/shared\:config.h
