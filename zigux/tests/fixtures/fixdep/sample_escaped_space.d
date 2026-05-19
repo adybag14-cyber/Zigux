68d8a9541b11fbc4b4b1c4380afbd05da95a7920
adybag14-cyber <adybag14@gmail.com>
2026-05-19 17:32:04 +0100

    test(zigux): widen escaped-space fixdep fixture rmeta parity

--- zigux/tests/fixtures/fixdep/sample_escaped_space.d
+++ zigux/tests/fixtures/fixdep/sample_escaped_space.d
@@ -3,4 +3,5 @@ sample_escaped_space.o: zigux/tests/fixtures/fixdep/sample_escaped_space_source.
  include/generated/autoconf.h \
  zigux/tests/fixtures/fixdep/sample2.so \
  zigux/tests/fixtures/fixdep/sample.rlib \
+ zigux/tests/fixtures/fixdep/sample.rmeta \
  zigux/tests/fixtures/fixdep/escaped\ space-config.h
