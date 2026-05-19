cb2c48ab1a9b766011b408fb744c566b5517f709
adybag14-cyber <adybag14@gmail.com>
2026-05-19 02:48:55 +0100

    test(scripts/zigux): prove abbreviated version argument failure

--- scripts/zigux/genksyms.zig
+++ scripts/zigux/genksyms.zig
@@ -879,6 +879,21 @@ test "genksyms bridge canonicalizes abbreviated dump-types empty inline argument
     }
 }
 
+test "genksyms bridge canonicalizes unexpected abbreviated version argument failures" {
+    const args = [_][]const u8{"--ver=extra"};
+    const outcome = try parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 0), failure.version_count);
+            switch (failure.reason) {
+                .unexpected_option_argument => |option| try testing.expectEqualStrings("--version", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.TestExpectedFailure,
+    }
+}
+
 test "genksyms bridge treats lone dash as positional passthrough" {
     var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
     defer arena_state.deinit();
