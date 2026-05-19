9df69d70b1a80e3fc0135f94b3f0a0d0391fedd4
adybag14-cyber <adybag14@gmail.com>
2026-05-19 07:03:01 +0100

    test(zigux): add Phase 7 argv_split fixture vectors

---
+++ zigux/tests/fixtures/phase7_argv_split_vectors.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+
+pub const ArgvSplitVector = struct {
+    name: []const u8,
+    input: []const u8,
+    expected_argc: usize,
+    expected_tokens: []const []const u8,
+    expect_empty_storage_view: bool = false,
+    expect_first_nul_truncation: bool = false,
+};
+
+pub const whitespace_tokens = [_][]const u8{
+    "alpha",
+    "beta",
+    "gamma",
+};
+
+pub const blank_tokens = [_][]const u8{};
+
+pub const first_nul_tokens = [_][]const u8{
+    "alpha",
+    "beta",
+};
+
+pub const quoted_tokens = [_][]const u8{
+    "alpha",
+    "\"beta",
+    "gamma\"",
+    "delta",
+};
+
+pub const phase7_argv_split_vectors = [_]ArgvSplitVector{
+    .{
+        .name = "copied_storage_whitespace_packet",
+        .input = " alpha  beta\tgamma\n",
+        .expected_argc = 3,
+        .expected_tokens = whitespace_tokens[0..],
+    },
+    .{
+        .name = "blank_input_reuses_empty_packet",
+        .input = " \t\n",
+        .expected_argc = 0,
+        .expected_tokens = blank_tokens[0..],
+        .expect_empty_storage_view = true,
+    },
+    .{
+        .name = "first_nul_truncation_keeps_tail_outside_packet",
+        .input = "alpha beta\x00ignored tail",
+        .expected_argc = 2,
+        .expected_tokens = first_nul_tokens[0..],
+        .expect_first_nul_truncation = true,
+    },
+    .{
+        .name = "quoted_tokens_stay_whitespace_split",
+        .input = "alpha \"beta gamma\" delta",
+        .expected_argc = 4,
+        .expected_tokens = quoted_tokens[0..],
+    },
+};
+
+test "phase 7 argv split fixture vectors stay reviewable" {
+    try std.testing.expectEqual(@as(usize, 4), phase7_argv_split_vectors.len);
+    try std.testing.expectEqualStrings("copied_storage_whitespace_packet", phase7_argv_split_vectors[0].name);
+    try std.testing.expectEqualStrings("blank_input_reuses_empty_packet", phase7_argv_split_vectors[1].name);
+    try std.testing.expectEqualStrings("first_nul_truncation_keeps_tail_outside_packet", phase7_argv_split_vectors[2].name);
+    try std.testing.expectEqualStrings("quoted_tokens_stay_whitespace_split", phase7_argv_split_vectors[3].name);
+    try std.testing.expect(phase7_argv_split_vectors[1].expect_empty_storage_view);
+    try std.testing.expect(phase7_argv_split_vectors[2].expect_first_nul_truncation);
+    try std.testing.expectEqualStrings("gamma", phase7_argv_split_vectors[0].expected_tokens[2]);
+    try std.testing.expectEqualStrings("beta", phase7_argv_split_vectors[2].expected_tokens[1]);
+    try std.testing.expectEqualStrings("\"beta", phase7_argv_split_vectors[3].expected_tokens[1]);
+}
