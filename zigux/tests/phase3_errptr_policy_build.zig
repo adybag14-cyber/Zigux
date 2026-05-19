8da176421bab1947ff322c5fdda36d22ae200ea4
adybag14-cyber <adybag14@gmail.com>
2026-05-19 21:18:59 +0100

    test(zigux): add lane04 errptr policy harness

---
+++ zigux/tests/phase3_errptr_policy_build.zig
@@ -0,0 +1,91 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const err_ptr = b.createModule(.{
+        .root_source_file = b.path("../helpers/err_ptr.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const xa_value = b.createModule(.{
+        .root_source_file = b.path("../helpers/xa_value.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    xa_value.addImport("err_ptr", err_ptr);
+
+    const errptr_root = b.createModule(.{
+        .root_source_file = b.path("phase3_errptr_xarray_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    errptr_root.addImport("err_ptr", err_ptr);
+    errptr_root.addImport("xa_value", xa_value);
+
+    const errptr_tests = b.addTest(.{
+        .root_module = errptr_root,
+    });
+    const run_errptr_tests = b.addRunArtifact(errptr_tests);
+
+    const abi_bindings = b.createModule(.{
+        .root_source_file = b.path("../bindings/abi.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const panic_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/panic_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    panic_policy.addImport("abi_bindings", abi_bindings);
+    const allocator_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/allocator_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    allocator_policy.addImport("abi_bindings", abi_bindings);
+    const unsafe_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/unsafe_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    unsafe_policy.addImport("abi_bindings", abi_bindings);
+    const layout_assert = b.createModule(.{
+        .root_source_file = b.path("../helpers/layout_assert.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    layout_assert.addImport("abi_bindings", abi_bindings);
+    const narrow_surface = b.createModule(.{
+        .root_source_file = b.path("../unsafe/narrow.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    narrow_surface.addImport("abi_bindings", abi_bindings);
+
+    const policy_root = b.createModule(.{
+        .root_source_file = b.path("phase3_policy_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    policy_root.addImport("abi_bindings", abi_bindings);
+    policy_root.addImport("panic_policy", panic_policy);
+    policy_root.addImport("allocator_policy", allocator_policy);
+    policy_root.addImport("unsafe_policy", unsafe_policy);
+    policy_root.addImport("layout_assert", layout_assert);
+    policy_root.addImport("narrow_surface", narrow_surface);
+
+    const policy_tests = b.addTest(.{
+        .root_module = policy_root,
+    });
+    const run_policy_tests = b.addRunArtifact(policy_tests);
+
+    const test_step = b.step(
+        "phase3-errptr-policy-test",
+        "Run the Phase 3 err_ptr/xarray and policy starter packets",
+    );
+    test_step.dependOn(&run_errptr_tests.step);
+    test_step.dependOn(&run_policy_tests.step);
+}
