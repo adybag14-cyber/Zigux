3a77c46b95f8f148dcf6949c1477b4977ff8c928
adybag14-cyber <adybag14@gmail.com>
2026-05-21 09:19:12 +0100

    test(zigux): restack lane04 xarray-helper export harness on current master

---
+++ zigux/tests/phase3_xarray_helper_export_build.zig
@@ -0,0 +1,138 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const err_ptr = b.createModule(.{
+        .root_source_file = b.path("../helpers/err_ptr.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const xa_value = b.createModule(.{
+        .root_source_file = b.path("../helpers/xa_value.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    xa_value.addImport("err_ptr", err_ptr);
+    const xarray_slot_view = b.createModule(.{
+        .root_source_file = b.path("../helpers/xarray_slot_view.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    xarray_slot_view.addImport("err_ptr", err_ptr);
+    xarray_slot_view.addImport("xa_value", xa_value);
+
+    const errptr_xarray_starter_root = b.createModule(.{
+        .root_source_file = b.path("phase3_errptr_xarray_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    errptr_xarray_starter_root.addImport("err_ptr", err_ptr);
+    errptr_xarray_starter_root.addImport("xa_value", xa_value);
+
+    const xarray_slot_starter_root = b.createModule(.{
+        .root_source_file = b.path("phase3_xarray_slot_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    xarray_slot_starter_root.addImport("err_ptr", err_ptr);
+    xarray_slot_starter_root.addImport("xa_value", xa_value);
+    xarray_slot_starter_root.addImport("xarray_slot_view", xarray_slot_view);
+
+    const errptr_xarray_dump_root = b.createModule(.{
+        .root_source_file = b.path("phase3_errptr_xarray_dump.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    errptr_xarray_dump_root.addImport("err_ptr", err_ptr);
+    errptr_xarray_dump_root.addImport("xa_value", xa_value);
+
+    const uapi_dev_t = b.createModule(.{
+        .root_source_file = b.path("../uapi/dev_t.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const uapi_version = b.createModule(.{
+        .root_source_file = b.path("../uapi/version.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const dev_t_binding = b.createModule(.{
+        .root_source_file = b.path("../bindings/dev_t.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    dev_t_binding.addImport("uapi_dev_t", uapi_dev_t);
+    const version_binding = b.createModule(.{
+        .root_source_file = b.path("../bindings/version.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    version_binding.addImport("uapi_version", uapi_version);
+    const abi_bindings = b.createModule(.{
+        .root_source_file = b.path("../bindings/abi.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const header_family_binding = b.createModule(.{
+        .root_source_file = b.path("../bindings/header_family.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    header_family_binding.addImport("abi_bindings", abi_bindings);
+    header_family_binding.addImport("dev_t_binding", dev_t_binding);
+    header_family_binding.addImport("version_binding", version_binding);
+    header_family_binding.addImport("uapi_version", uapi_version);
+    const export_shim = b.createModule(.{
+        .root_source_file = b.path("../kernel/export_shim.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    export_shim.addImport("abi_bindings", abi_bindings);
+    export_shim.addImport("dev_t_binding", dev_t_binding);
+    export_shim.addImport("version_binding", version_binding);
+
+    const export_root = b.createModule(.{
+        .root_source_file = b.path("phase3_export_uapi_layout.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    export_root.addImport("uapi_dev_t", uapi_dev_t);
+    export_root.addImport("uapi_version", uapi_version);
+    export_root.addImport("dev_t_binding", dev_t_binding);
+    export_root.addImport("version_binding", version_binding);
+    export_root.addImport("header_family_binding", header_family_binding);
+    export_root.addImport("export_shim", export_shim);
+
+    const starter_tests = b.addTest(.{
+        .name = "phase3-errptr-xarray-starter-packet",
+        .root_module = errptr_xarray_starter_root,
+    });
+    const xarray_slot_tests = b.addTest(.{
+        .name = "phase3-xarray-slot-starter-packet",
+        .root_module = xarray_slot_starter_root,
+    });
+    const dump_exe = b.addExecutable(.{
+        .name = "phase3-errptr-xarray-dump",
+        .root_module = errptr_xarray_dump_root,
+    });
+    const export_tests = b.addTest(.{
+        .name = "phase3-export-uapi-layout",
+        .root_module = export_root,
+    });
+
+    const run_starter_tests = b.addRunArtifact(starter_tests);
+    const run_xarray_slot_tests = b.addRunArtifact(xarray_slot_tests);
+    const run_dump = b.addRunArtifact(dump_exe);
+    const run_export_tests = b.addRunArtifact(export_tests);
+
+    const test_step = b.step(
+        "phase3-xarray-helper-export-test",
+        "Run the Phase 3 xarray helper cluster and export/UAPI layout replay",
+    );
+    test_step.dependOn(&run_starter_tests.step);
+    test_step.dependOn(&run_xarray_slot_tests.step);
+    test_step.dependOn(&run_dump.step);
+    test_step.dependOn(&run_export_tests.step);
+}
