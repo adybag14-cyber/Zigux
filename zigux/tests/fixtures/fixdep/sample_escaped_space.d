ac127897ca912e23674d292ed9d4f9b3702ef904
adybag14-cyber <adybag14@gmail.com>
2026-05-22 17:32:51 +0100

    test(zigux): widen fixdep escaped-space tail

--- zigux/tests/fixtures/fixdep/sample_escaped_space.d
+++ zigux/tests/fixtures/fixdep/sample_escaped_space.d
@@ -1,2 +1,3 @@
 sample_escaped_space.o: zigux/tests/fixtures/fixdep/sample_escaped_space_source.rmeta \
+ zigux/tests/fixtures/fixdep/dep\ name.rmeta include/generated/autoconf.h \
  zigux/tests/fixtures/fixdep/dep\ name.rmeta
