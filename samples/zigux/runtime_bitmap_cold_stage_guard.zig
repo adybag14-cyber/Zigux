f7af734974a36d699f5d30109c766cb3eb7f7f9b
adybag14-cyber <adybag14@gmail.com>
2026-05-23 22:52:53 +0100

    test(phase9): cover bitmap exited-stage guard rollback

--- samples/zigux/runtime_bitmap_cold_stage_guard.zig
+++ samples/zigux/runtime_bitmap_cold_stage_guard.zig
@@ -73,3 +73,74 @@ test "runtime bitmap sample keeps cold-stage mutation guards and source-lifecycl
     try std.testing.expectEqual(@as(?u32, 65), destination.nthSetBit(1));
     try std.testing.expectEqual(@as(?u32, null), destination.nthSetBit(2));
 }
+
+test "runtime bitmap sample keeps exited-stage mutation and second-exit guards explicit" {
+    var initialized_module = RuntimeBitmapSample{};
+    try initialized_module.initFromBitList("0, 63, 64, 127");
+    try initialized_module.exit();
+
+    const before_initialized_reexit = initialized_module.summary();
+    try std.testing.expectEqual(ModuleStage.exited, initialized_module.stage());
+    try std.testing.expectEqual(@as(u32, 0), before_initialized_reexit.first_set);
+    try std.testing.expectEqual(@as(u32, 1), before_initialized_reexit.first_zero);
+    try std.testing.expectEqual(@as(u32, 4), before_initialized_reexit.weight);
+    try std.testing.expectEqual(RuntimeBitmapSample.bitmap_nbits, before_initialized_reexit.nbits);
+    try std.testing.expectEqual(@as(usize, 1), before_initialized_reexit.init_runs);
+    try std.testing.expectEqual(@as(usize, 0), before_initialized_reexit.selftest_runs);
+    try std.testing.expectEqual(@as(usize, 1), before_initialized_reexit.exit_runs);
+    try std.testing.expect(initialized_module.isSet(0));
+    try std.testing.expect(initialized_module.isSet(63));
+    try std.testing.expect(initialized_module.isSet(64));
+    try std.testing.expect(initialized_module.isSet(127));
+    try std.testing.expectEqual(@as(?u32, 127), initialized_module.nthSetBit(3));
+    try std.testing.expectEqual(@as(u32, 4), try initialized_module.countSetBitsInRange(0, RuntimeBitmapSample.bitmap_nbits));
+
+    try std.testing.expectError(error.InvalidLifecycleTransition, initialized_module.setRange(5, 1));
+    try std.testing.expectError(error.InvalidLifecycleTransition, initialized_module.clearRange(63, 1));
+    try std.testing.expectError(error.InvalidLifecycleTransition, initialized_module.exit());
+
+    const after_initialized_reexit = initialized_module.summary();
+    try std.testing.expectEqual(ModuleStage.exited, initialized_module.stage());
+    try expectSummaryStable(before_initialized_reexit, after_initialized_reexit);
+    try std.testing.expect(initialized_module.isSet(0));
+    try std.testing.expect(initialized_module.isSet(63));
+    try std.testing.expect(initialized_module.isSet(64));
+    try std.testing.expect(initialized_module.isSet(127));
+    try std.testing.expectEqual(@as(?u32, 127), initialized_module.nthSetBit(3));
+    try std.testing.expectEqual(@as(u32, 4), try initialized_module.countSetBitsInRange(0, RuntimeBitmapSample.bitmap_nbits));
+
+    var selftested_module = RuntimeBitmapSample{};
+    try selftested_module.initFromBitList("0, 63, 64, 127");
+    _ = try selftested_module.runSelftest();
+    try selftested_module.exit();
+
+    const before_selftested_reexit = selftested_module.summary();
+    try std.testing.expectEqual(ModuleStage.exited, selftested_module.stage());
+    try std.testing.expectEqual(@as(u32, 0), before_selftested_reexit.first_set);
+    try std.testing.expectEqual(@as(u32, 1), before_selftested_reexit.first_zero);
+    try std.testing.expectEqual(@as(u32, 4), before_selftested_reexit.weight);
+    try std.testing.expectEqual(RuntimeBitmapSample.bitmap_nbits, before_selftested_reexit.nbits);
+    try std.testing.expectEqual(@as(usize, 1), before_selftested_reexit.init_runs);
+    try std.testing.expectEqual(@as(usize, 1), before_selftested_reexit.selftest_runs);
+    try std.testing.expectEqual(@as(usize, 1), before_selftested_reexit.exit_runs);
+    try std.testing.expect(selftested_module.isSet(0));
+    try std.testing.expect(selftested_module.isSet(63));
+    try std.testing.expect(selftested_module.isSet(64));
+    try std.testing.expect(selftested_module.isSet(127));
+    try std.testing.expectEqual(@as(?u32, 127), selftested_module.nthSetBit(3));
+    try std.testing.expectEqual(@as(u32, 4), try selftested_module.countSetBitsInRange(0, RuntimeBitmapSample.bitmap_nbits));
+
+    try std.testing.expectError(error.InvalidLifecycleTransition, selftested_module.setRange(5, 1));
+    try std.testing.expectError(error.InvalidLifecycleTransition, selftested_module.clearRange(63, 1));
+    try std.testing.expectError(error.InvalidLifecycleTransition, selftested_module.exit());
+
+    const after_selftested_reexit = selftested_module.summary();
+    try std.testing.expectEqual(ModuleStage.exited, selftested_module.stage());
+    try expectSummaryStable(before_selftested_reexit, after_selftested_reexit);
+    try std.testing.expect(selftested_module.isSet(0));
+    try std.testing.expect(selftested_module.isSet(63));
+    try std.testing.expect(selftested_module.isSet(64));
+    try std.testing.expect(selftested_module.isSet(127));
+    try std.testing.expectEqual(@as(?u32, 127), selftested_module.nthSetBit(3));
+    try std.testing.expectEqual(@as(u32, 4), try selftested_module.countSetBitsInRange(0, RuntimeBitmapSample.bitmap_nbits));
+}
