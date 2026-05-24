ea8b6a3e3d821d164994e7be2bce53e1b2d98bbc
adybag14-cyber <adybag14@gmail.com>
2026-05-24 05:57:48 +0100

    test(scripts/zigux): restack lane23 repeated abbreviated help proof

---
+++ scripts/zigux/genksyms_repeated_abbreviated_version_before_help_test.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const testing = std.testing;
+const genksyms = @import("genksyms.zig");
+
+test "genksyms bridge preserves repeated abbreviated version side effects before short help" {
+    const args = [_][]const u8{
+        "--ver",
+        "--ver",
+        "-h",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .help => |version_count| try testing.expectEqual(@as(usize, 2), version_count),
+            else => return error.ExpectedHelpCommand,
+        },
+        else => return error.ExpectedCommand,
+    }
+}
+
+test "genksyms bridge preserves repeated abbreviated version side effects before long help" {
+    const args = [_][]const u8{
+        "--ver",
+        "-V",
+        "--help",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .help => |version_count| try testing.expectEqual(@as(usize, 2), version_count),
+            else => return error.ExpectedHelpCommand,
+        },
+        else => return error.ExpectedCommand,
+    }
+}
