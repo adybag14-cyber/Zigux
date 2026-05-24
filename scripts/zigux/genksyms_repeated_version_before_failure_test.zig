6b6290f646984e40ca994a3cf7a03625fcc2fdd6
adybag14-cyber <adybag14@gmail.com>
2026-05-24 13:54:31 +0100

    test(scripts/zigux): add lane23 repeated-version-before-failure proof

---
+++ scripts/zigux/genksyms_repeated_version_before_failure_test.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const testing = std.testing;
+const genksyms = @import("./genksyms.zig");
+
+test "genksyms bridge keeps repeated version side effects before invalid short option" {
+    const args = [_][]const u8{
+        "--version",
+        "--ver",
+        "-x",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 2), failure.version_count);
+            switch (failure.reason) {
+                .invalid_option => |option| try testing.expectEqualStrings("x", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
+
+test "genksyms bridge keeps repeated version side effects before ambiguous long option" {
+    const args = [_][]const u8{
+        "-V",
+        "--version",
+        "--d",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 2), failure.version_count);
+            switch (failure.reason) {
+                .ambiguous_option => |option| try testing.expectEqualStrings("--d", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
