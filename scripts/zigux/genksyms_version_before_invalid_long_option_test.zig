b542b41eacfe280230a084eddb932ecd57a02e64
adybag14-cyber <adybag14@gmail.com>
2026-05-26 02:46:53 +0100

    test(zigux): extend genksyms version side-effect proof

--- scripts/zigux/genksyms_version_before_invalid_long_option_test.zig
+++ scripts/zigux/genksyms_version_before_invalid_long_option_test.zig
@@ -37,3 +37,39 @@ test "genksyms bridge preserves abbreviated version side effect before invalid l
         else => return error.ExpectedFailure,
     }
 }
+
+test "genksyms bridge preserves version side effect before abbreviated unexpected long option argument" {
+    const args = [_][]const u8{
+        "--version",
+        "--he=extra",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            switch (failure.reason) {
+                .unexpected_option_argument => |option| try testing.expectEqualStrings("--help", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
+
+test "genksyms bridge preserves abbreviated version side effect before abbreviated unexpected long option argument" {
+    const args = [_][]const u8{
+        "--ver",
+        "--he=extra",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            switch (failure.reason) {
+                .unexpected_option_argument => |option| try testing.expectEqualStrings("--help", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
