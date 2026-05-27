6f3cc82b69bf6963e9f54067898ed5c132b8e852
adybag14-cyber <adybag14@gmail.com>
2026-05-27 22:49:46 +0100

    test(scripts/zigux): add lane23 version-before-missing-long-dump-types proof

---
+++ scripts/zigux/genksyms_version_before_missing_long_dump_types_argument_test.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+const testing = std.testing;
+const genksyms = @import("genksyms.zig");
+
+test "genksyms bridge preserves version side effect before missing long dump-types argument" {
+    const args = [_][]const u8{
+        "--version",
+        "--dump-types",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            switch (failure.reason) {
+                .missing_option_argument => |option| try testing.expectEqualStrings("--dump-types", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
+
+test "genksyms bridge preserves abbreviated version side effect before abbreviated missing long dump-types argument" {
+    const args = [_][]const u8{
+        "--ver",
+        "--dump-t",
+    };
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            switch (failure.reason) {
+                .missing_option_argument => |option| try testing.expectEqualStrings("--dump-types", option),
+                else => return error.UnexpectedParseFailure,
+            }
+        },
+        else => return error.ExpectedFailure,
+    }
+}
