3d9863a58e9424327975c7a0f00ed74263dd4f72
adybag14-cyber <adybag14@gmail.com>
2026-05-27 11:33:14 +0100

    test(zigux): widen fixdep multi-target comment tail

--- zigux/tests/fixtures/fixdep/sample_multi_target.d
+++ zigux/tests/fixtures/fixdep/sample_multi_target.d
@@ -17,4 +17,7 @@ module/sample2.o module/sample2.d: zigux/tests/fixtures/fixdep/sample2.c \
  zigux/tests/fixtures/fixdep/sample2-config.h \
  include/generated/autoconf.h \
  zigux/tests/fixtures/fixdep/shared\:config.h
-# comment line that should be ignored
+# comment line that should be ignored \
+still comment with zigux/tests/fixtures/fixdep/escaped\ space-config.h zigux/tests/fixtures/fixdep/shared\#config.h \
+and zigux/tests/fixtures/fixdep/shared\:config.h zigux/tests/fixtures/fixdep/sample2.so sample.rlib \
+plus sample.rmeta include/generated/autoconf.h zigux/tests/fixtures/fixdep/sample2-config.h stay ignored too
