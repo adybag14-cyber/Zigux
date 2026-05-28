0d000ad0db2fd78de47dec84777d7136c798fab4
adybag14-cyber <adybag14@gmail.com>
2026-05-29 00:50:42 +0100

    test(scripts/zigux): add genksyms version reference-limit proof

---
+++ scripts/zigux/genksyms_version_before_too_many_references_test.zig
@@ -0,0 +1,101 @@
+const std = @import("std");
+const testing = std.testing;
+const genksyms = @import("genksyms.zig");
+
+test "genksyms bridge preserves version side effect before too many references" {
+    const args = [_][]const u8{
+        "--version",
+        "-r",
+        "01.symref",
+        "-r",
+        "02.symref",
+        "-r",
+        "03.symref",
+        "-r",
+        "04.symref",
+        "-r",
+        "05.symref",
+        "-r",
+        "06.symref",
+        "-r",
+        "07.symref",
+        "-r",
+        "08.symref",
+        "-r",
+        "09.symref",
+        "-r",
+        "10.symref",
+        "-r",
+        "11.symref",
+        "-r",
+        "12.symref",
+        "-r",
+        "13.symref",
+        "-r",
+        "14.symref",
+        "-r",
+        "15.symref",
+        "-r",
+        "16.symref",
+        "-r",
+        "17.symref",
+    };
+
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            try testing.expectEqual(genksyms.ParseFailure.too_many_reference_files, failure.reason);
+        },
+        else => return error.ExpectedFailure,
+    }
+}
+
+test "genksyms bridge preserves abbreviated version side effect before too many references" {
+    const args = [_][]const u8{
+        "--ver",
+        "--reference",
+        "01.symref",
+        "--reference",
+        "02.symref",
+        "--reference",
+        "03.symref",
+        "--reference",
+        "04.symref",
+        "--reference",
+        "05.symref",
+        "--reference",
+        "06.symref",
+        "--reference",
+        "07.symref",
+        "--reference",
+        "08.symref",
+        "--reference",
+        "09.symref",
+        "--reference",
+        "10.symref",
+        "--reference",
+        "11.symref",
+        "--reference",
+        "12.symref",
+        "--reference",
+        "13.symref",
+        "--reference",
+        "14.symref",
+        "--reference",
+        "15.symref",
+        "--reference",
+        "16.symref",
+        "--reference",
+        "17.symref",
+    };
+
+    const outcome = try genksyms.parseArgs(testing.allocator, &args);
+    switch (outcome) {
+        .failure => |failure| {
+            try testing.expectEqual(@as(usize, 1), failure.version_count);
+            try testing.expectEqual(genksyms.ParseFailure.too_many_reference_files, failure.reason);
+        },
+        else => return error.ExpectedFailure,
+    }
+}
