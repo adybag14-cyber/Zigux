faef9319fb4c11fa30f8f0a7fa297683ca9811ab
adybag14-cyber <adybag14@gmail.com>
2026-05-28 19:04:12 +0100

    test(zigux): add lane28 list/hlist policy build shard

---
+++ zigux/tests/phase3_list_hlist_policy_build.zig
@@ -0,0 +1,102 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const list_view = b.createModule(.{
+        .root_source_file = b.path("../helpers/list_view.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const hlist_view = b.createModule(.{
+        .root_source_file = b.path("../helpers/hlist_view.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const list_hlist_root = b.createModule(.{
+        .root_source_file = b.path("phase3_list_hlist_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    list_hlist_root.addImport("list_view", list_view);
+    list_hlist_root.addImport("hlist_view", hlist_view);
+
+    const notifier_abi = b.createModule(.{
+        .root_source_file = b.path("../bindings/notifier_abi.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const abi_bindings = b.createModule(.{
+        .root_source_file = b.path("../bindings/abi.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    abi_bindings.addImport("notifier_abi", notifier_abi);
+
+    const narrow_surface = b.createModule(.{
+        .root_source_file = b.path("../unsafe/narrow.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    narrow_surface.addImport("abi_bindings", abi_bindings);
+
+    const allocator_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/allocator_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    allocator_policy.addImport("abi_bindings", abi_bindings);
+
+    const panic_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/panic_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    panic_policy.addImport("abi_bindings", abi_bindings);
+
+    const unsafe_policy = b.createModule(.{
+        .root_source_file = b.path("../helpers/unsafe_policy.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    unsafe_policy.addImport("abi_bindings", abi_bindings);
+    unsafe_policy.addImport("narrow", narrow_surface);
+
+    const layout_assert = b.createModule(.{
+        .root_source_file = b.path("../helpers/layout_assert.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    layout_assert.addImport("abi_bindings", abi_bindings);
+
+    const policy_root = b.createModule(.{
+        .root_source_file = b.path("phase3_policy_starter_packet.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    policy_root.addImport("abi_bindings", abi_bindings);
+    policy_root.addImport("allocator_policy", allocator_policy);
+    policy_root.addImport("panic_policy", panic_policy);
+    policy_root.addImport("unsafe_policy", unsafe_policy);
+    policy_root.addImport("layout_assert", layout_assert);
+    policy_root.addImport("narrow_surface", narrow_surface);
+
+    const list_hlist_tests = b.addTest(.{
+        .root_module = list_hlist_root,
+    });
+    const run_list_hlist_tests = b.addRunArtifact(list_hlist_tests);
+
+    const policy_tests = b.addTest(.{
+        .root_module = policy_root,
+    });
+    const run_policy_tests = b.addRunArtifact(policy_tests);
+
+    const test_step = b.step(
+        "phase3-list-hlist-policy-test",
+        "Run the Phase 3 list/hlist and policy starter packets together",
+    );
+    test_step.dependOn(&run_list_hlist_tests.step);
+    test_step.dependOn(&run_policy_tests.step);
+}
