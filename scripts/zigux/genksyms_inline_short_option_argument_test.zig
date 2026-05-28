0a93eff9e601830f91f9bba0f9dfe16365820464
adybag14-cyber <adybag14@gmail.com>
2026-05-28 03:25:41 +0100

    test(scripts/zigux): pin inline short genksyms bridge args

---
+++ scripts/zigux/genksyms_inline_short_option_argument_test.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const testing = std.testing;
+const genksyms = @import("genksyms.zig");
+
+test "genksyms bridge accepts inline short option arguments" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "-d",
+        "-rfoo.symref",
+        "-Ttypes.symtypes",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.debug_level);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("foo.symref", request.reference_files[0]);
+                try testing.expectEqualStrings("types.symtypes", request.dump_types_file.?);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
