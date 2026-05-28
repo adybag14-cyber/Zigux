794268a326f1baccc50017675a84429fec50d627
adybag14-cyber <adybag14@gmail.com>
2026-05-28 20:53:58 +0100

    test(scripts/zigux): widen genksyms inline short proof

--- scripts/zigux/genksyms_inline_short_option_argument_test.zig
+++ scripts/zigux/genksyms_inline_short_option_argument_test.zig
@@ -26,3 +26,30 @@ test "genksyms bridge accepts inline short option arguments" {
         else => return error.ExpectedRequestCommand,
     }
 }
+
+test "genksyms bridge accepts clustered flags before inline short option arguments" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "-dwprfoo.symref",
+        "-DqTtypes.symtypes",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.debug_level);
+                try testing.expect(!request.warnings);
+                try testing.expect(request.dump_defs);
+                try testing.expect(request.preserve);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("foo.symref", request.reference_files[0]);
+                try testing.expectEqualStrings("types.symtypes", request.dump_types_file.?);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
