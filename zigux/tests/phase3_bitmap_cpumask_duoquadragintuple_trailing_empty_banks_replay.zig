f0a04900639a198196ee226313ba83eafc7469d2
adybag14-cyber <adybag14@gmail.com>
2026-05-28 12:01:07 +0100

    test(zigux): add lane27 duoquadragintuple trailing empty-banks replay

---
+++ zigux/tests/phase3_bitmap_cpumask_duoquadragintuple_trailing_empty_banks_replay.zig
@@ -0,0 +1,101 @@
+const std = @import("std");
+const testing = std.testing;
+
+const bitmap_view = @import("bitmap_view");
+const cpumask_view = @import("cpumask_view");
+
+fn bit(index: usize) usize {
+    return @as(usize, 1) << @intCast(index);
+}
+
+test "duoquadragintuple trailing empty banks keep early windows aligned and masked tail bits bounded" {
+    const capacity = (44 * bitmap_view.word_bits) + 11;
+    const words = [_]usize{
+        bit(0) | bit(5) | bit(9) | bit(12),
+        bit(2) | bit(8) | bit(11) | bit(15),
+    } ++ [_]usize{0} ** 42 ++ [_]usize{
+        bit(1) | bit(6) | bit(10) | bit(14),
+        std.math.maxInt(usize),
+    };
+
+    const bitmap = bitmap_view.BitmapView.init(words[0..], capacity);
+    const cpumask = cpumask_view.CpuMaskView.init(words[0..], capacity);
+
+    try testing.expect(bitmap.isSet(0));
+    try testing.expect(!bitmap.isSet(1));
+    try testing.expect(bitmap.isSet(5));
+    try testing.expect(bitmap.isSet(bitmap_view.word_bits + 8));
+    try testing.expect(!bitmap.isSet((2 * bitmap_view.word_bits) + 3));
+    try testing.expect(bitmap.isSet((44 * bitmap_view.word_bits) + 1));
+    try testing.expect(bitmap.isSet((44 * bitmap_view.word_bits) + 10));
+    try testing.expect(!bitmap.isSet((44 * bitmap_view.word_bits) + 9));
+    try testing.expectEqual(@as(usize, 11), bitmap.countSetBits());
+    try testing.expectEqual(bitmap.countSetBits(), cpumask.countPresentCpus());
+    try testing.expectEqual(@as(?usize, 0), bitmap.firstSetBit());
+    try testing.expectEqual(bitmap.firstSetBit(), cpumask.firstCpu());
+    try testing.expectEqual(@as(?usize, 1), bitmap.firstClearBit());
+    try testing.expectEqual(bitmap.firstClearBit(), cpumask.firstMissingCpu());
+    try testing.expect(cpumask.hasCpu(bitmap_view.word_bits + 11));
+    try testing.expect(!cpumask.hasCpu((43 * bitmap_view.word_bits) + 7));
+    try testing.expect(cpumask.hasCpu((44 * bitmap_view.word_bits) + 6));
+}
+
+test "duoquadragintuple trailing empty banks keep subset and overlap checks bounded under masked late noise" {
+    const capacity = (44 * bitmap_view.word_bits) + 11;
+    const base_words = [_]usize{
+        bit(0) | bit(5) | bit(9) | bit(12),
+        bit(2) | bit(8) | bit(11) | bit(15),
+    } ++ [_]usize{0} ** 42 ++ [_]usize{
+        bit(1) | bit(6) | bit(10) | bit(14),
+        std.math.maxInt(usize),
+    };
+    const subset_words = [_]usize{
+        bit(0) | bit(9),
+        bit(8) | bit(15),
+    } ++ [_]usize{0} ** 42 ++ [_]usize{
+        bit(1) | bit(10),
+        std.math.maxInt(usize),
+    };
+    const overlap_words = [_]usize{
+        bit(5),
+        bit(11),
+    } ++ [_]usize{0} ** 42 ++ [_]usize{
+        bit(6) | bit(13),
+        std.math.maxInt(usize),
+    };
+    const disjoint_words = [_]usize{
+        bit(3) | bit(7),
+        bit(1) | bit(4),
+    } ++ [_]usize{0} ** 42 ++ [_]usize{
+        bit(4),
+        std.math.maxInt(usize),
+    };
+    const tail_only_words = [_]usize{0} ** 44 ++ [_]usize{
+        bit(12) | bit(13) | bit(15),
+        std.math.maxInt(usize),
+    };
+
+    const base = cpumask_view.CpuMaskView.init(base_words[0..], capacity);
+    const subset = cpumask_view.CpuMaskView.init(subset_words[0..], capacity);
+    const overlap = cpumask_view.CpuMaskView.init(overlap_words[0..], capacity);
+    const disjoint = cpumask_view.CpuMaskView.init(disjoint_words[0..], capacity);
+    const tail_only = cpumask_view.CpuMaskView.init(tail_only_words[0..], capacity);
+    const tail_only_bitmap = bitmap_view.BitmapView.init(tail_only_words[0..], capacity);
+
+    try testing.expect(subset.isSubsetOf(base));
+    try testing.expect(!base.isSubsetOf(subset));
+    try testing.expect(base.intersects(overlap));
+    try testing.expect(overlap.intersects(base));
+    try testing.expect(!disjoint.isSubsetOf(base));
+    try testing.expect(!base.intersects(disjoint));
+    try testing.expect(!disjoint.intersects(base));
+    try testing.expect(tail_only.isSubsetOf(base));
+    try testing.expect(!base.intersects(tail_only));
+    try testing.expect(!tail_only.intersects(base));
+    try testing.expectEqual(@as(usize, 0), tail_only_bitmap.countSetBits());
+    try testing.expectEqual(@as(?usize, null), tail_only_bitmap.firstSetBit());
+    try testing.expectEqual(@as(?usize, 0), tail_only_bitmap.firstClearBit());
+    try testing.expectEqual(@as(usize, 0), tail_only.countPresentCpus());
+    try testing.expectEqual(@as(?usize, null), tail_only.firstCpu());
+    try testing.expectEqual(@as(?usize, 0), tail_only.firstMissingCpu());
+}
