20b87c89b65c7e5508aa382d1a56db2ee30fb57f
adybag14-cyber <adybag14@gmail.com>
2026-05-29 18:53:09 +0100

    test(scripts/zigux): add inline option-like required argument proof

---
+++ scripts/zigux/genksyms_inline_option_like_required_argument_test.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+const testing = std.testing;
+
+const genksyms = @import("genksyms.zig");
+
+test "inline long required arguments keep option-like values as data" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "--reference=--version",
+        "--dump-types=--help",
+        "--version",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.version_count);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("--version", request.reference_files[0]);
+                try testing.expectEqualStrings("--help", request.dump_types_file.?);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
+
+test "inline short required arguments keep option-like values as data" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "-r--help",
+        "-T-V",
+        "-V",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.version_count);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("--help", request.reference_files[0]);
+                try testing.expectEqualStrings("-V", request.dump_types_file.?);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
+
+test "inline short required argument stops the current option cluster" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "-r-Vd",
+        "-d",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.debug_level);
+                try testing.expectEqual(@as(usize, 0), request.version_count);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("-Vd", request.reference_files[0]);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
