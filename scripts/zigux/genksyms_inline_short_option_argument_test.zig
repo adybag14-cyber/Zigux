4799b04abba5776f9f25224fc90b38b3997ec645
adybag14-cyber <adybag14@gmail.com>
2026-05-30 02:43:40 +0100

    test(scripts/zigux): pin genksyms short option cluster args

--- scripts/zigux/genksyms_inline_short_option_argument_test.zig
+++ scripts/zigux/genksyms_inline_short_option_argument_test.zig
@@ -26,3 +26,28 @@ test "genksyms bridge accepts inline short option arguments" {
         else => return error.ExpectedRequestCommand,
     }
 }
+
+test "genksyms bridge accepts clustered short option argument after version" {
+    var arena_state = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena_state.deinit();
+
+    const args = [_][]const u8{
+        "-Vrfoo.symref",
+        "-Ttypes.symtypes",
+    };
+    const outcome = try genksyms.parseArgs(arena_state.allocator(), &args);
+    switch (outcome) {
+        .command => |command| switch (command) {
+            .request => |request| {
+                try testing.expectEqual(@as(usize, 1), request.version_count);
+                try testing.expectEqual(@as(usize, 0), request.debug_level);
+                try testing.expectEqual(@as(usize, 1), request.reference_files.len);
+                try testing.expectEqualStrings("foo.symref", request.reference_files[0]);
+                try testing.expectEqualStrings("types.symtypes", request.dump_types_file.?);
+                try testing.expectEqualSlices([]const u8, &args, request.rendered_args);
+            },
+            else => return error.ExpectedRequestCommand,
+        },
+        else => return error.ExpectedRequestCommand,
+    }
+}
