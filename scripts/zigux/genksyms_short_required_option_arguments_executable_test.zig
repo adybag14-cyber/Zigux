b0a68a9596e6b03c2fd96a63882d8f75448e37ec
adybag14-cyber <adybag14@gmail.com>
2026-06-08 06:50:03 +0100

    test(scripts/zigux): restack short required genksyms proof

---
+++ scripts/zigux/genksyms_short_required_option_arguments_executable_test.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+
+const testing = std.testing;
+
+fn expectExitedZero(term: std.process.Child.Term) !void {
+    switch (term) {
+        .exited => |code| try testing.expectEqual(@as(u8, 0), code),
+        else => return error.ExpectedExitedZero,
+    }
+}
+
+test "genksyms executable keeps short required option arguments as data" {
+    var tmp = testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    var exe_path_buffer: [256]u8 = undefined;
+    const exe_path = try std.fmt.bufPrint(
+        &exe_path_buffer,
+        ".zig-cache/tmp/{s}/genksyms-short-required-args",
+        .{tmp.sub_path},
+    );
+    const emit_arg = try std.fmt.allocPrint(testing.allocator, "-femit-bin={s}", .{exe_path});
+    defer testing.allocator.free(emit_arg);
+
+    const build_result = try std.process.run(testing.allocator, testing.io, .{
+        .argv = &.{
+            "zig",
+            "build-exe",
+            "scripts/zigux/genksyms.zig",
+            emit_arg,
+        },
+        .stdout_limit = .limited(4096),
+        .stderr_limit = .limited(4096),
+    });
+    defer testing.allocator.free(build_result.stdout);
+    defer testing.allocator.free(build_result.stderr);
+
+    try expectExitedZero(build_result.term);
+    try testing.expectEqualStrings("", build_result.stdout);
+    try testing.expectEqualStrings("", build_result.stderr);
+
+    const run_result = try std.process.run(testing.allocator, testing.io, .{
+        .argv = &.{
+            exe_path,
+            "-r",
+            "-d",
+            "-T",
+            "-p",
+            "-w",
+            "unit.c",
+        },
+        .stdout_limit = .limited(4096),
+        .stderr_limit = .limited(4096),
+    });
+    defer testing.allocator.free(run_result.stdout);
+    defer testing.allocator.free(run_result.stderr);
+
+    try expectExitedZero(run_result.term);
+    try testing.expectEqualStrings("", run_result.stderr);
+    try testing.expectEqualStrings(
+        "{\"tool\":\"scripts/genksyms/genksyms\",\"stdin\":\"cpp-stream\",\"stdout\":\"symversions\",\"argv\":[\"scripts/genksyms/genksyms\",\"-r\",\"-d\",\"-T\",\"-p\",\"-w\",\"unit.c\"],\"options\":{\"debug_level\":0,\"warnings\":true,\"dump_defs\":false,\"preserve\":false,\"reference_files\":[\"-d\"],\"dump_types_file\":\"-p\"}}\n",
+        run_result.stdout,
+    );
+}
